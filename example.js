71e85d8b564f4e1f1cca0014d254e628e348e4f0
Matt <carlotta.matt@gmail.com>
2019-12-26 02:03:44 -0800

    Initial commit

---
+++ example.js
@@ -0,0 +1,49 @@
+class H1LikeButton extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { isLiked: false };
+    this.handleClick = this.handleClick.bind(this);
+  }
+
+  handleClick() {
+    this.setState(prevState => ({ isLiked: !prevState.isLiked }));
+  }
+
+  render() {
+    return (
+      <React.Fragment>
+        {this.state.isLiked && <p>This is liked.</p>}
+        <h1 onClick={this.handleClick}>
+          {this.state.isLiked ? "Like" : "Dislike"}
+        </h1>
+      </React.Fragment>
+    );
+  }
+}
+
+class LikeButton extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { isLiked: false };
+    this.handleClick = this.handleClick.bind(this);
+  }
+
+  handleClick() {
+    this.setState(prevState => ({ isLiked: !prevState.isLiked }));
+  }
+
+  render() {
+    return (
+      <div>
+        {this.state.isLiked && <p>This is liked.</p>}
+        <button onClick={this.handleClick}>
+          {this.state.isLiked ? "Like" : "Dislike"}
+        </button>
+        <br />
+        <H1LikeButton />
+      </div>
+    );
+  }
+}
+
+ReactDOM.render(<LikeButton />, document.getElementById("#root"));
---
+++ main.js
@@ -0,0 +1,2 @@
+requirejs.config({paths: {'react': 'https://unpkg.com/react@16/umd/react.production.min','react-dom': 'https://unpkg.com/react-dom@16/umd/react-dom.production.min'}});
+
